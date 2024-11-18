b0844c8adf422656cc8ef8a4837bbfb00c465fb9
Mosha Langerak <82883699+MoshaLangerak@users.noreply.github.com>
2024-11-18 08:49:21 +0100

    Update sudokuai.py

--- team11_A1/sudokuai.py
+++ team11_A1/sudokuai.py
@@ -16,21 +16,14 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     def __init__(self):
         super().__init__()
 
-    # N.B. This is a very naive implementation.
+    def evaluate(self, game_state: GameState):
+        return None
+    
+    def getChildren(self, game_state: GameState):
+        return None
+    
+    def minimax(self, game_state: GameState, depth, maximizingPlayer):
+        return None
+    
     def compute_best_move(self, game_state: GameState) -> None:
-        N = game_state.board.N
-
-        # Check whether a cell is empty, a value in that cell is not taboo, and that cell is allowed
-        def possible(i, j, value):
-            return game_state.board.get((i, j)) == SudokuBoard.empty \
-                   and not TabooMove((i, j), value) in game_state.taboo_moves \
-                       and (i, j) in game_state.player_squares()
-
-        all_moves = [Move((i, j), value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
-        move = random.choice(all_moves)
-        self.propose_move(move)
-        while True:
-            time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
-
+        return None
